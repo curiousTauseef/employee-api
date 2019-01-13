0c1c3262b791ef5cc0efa1e918461c352aa179a3
Jason Lai <jase_lai@hotmail.com>
2019-01-13 01:46:20 +0000

    add eslint pretter

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { shallow } from 'enzyme';
-
-
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
 
 describe('<App/>', () => {
     it('renders without crashing', () => {
