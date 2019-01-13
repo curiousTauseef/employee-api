6d752090147770817b17597ef9cc1e4e444c2880
Jason Lai <jase_lai@hotmail.com>
2019-01-13 23:22:47 +0000

    test passing for failed fetch

--- src/__test__/Fetch.test.js
+++ src/__test__/Fetch.test.js
@@ -6,7 +6,7 @@ import Fetch from '../components/Fetch'
 import { mockData } from '../lib/testUtils'
 
 describe('<Fetch />', () => {
-    it('Renders employee data and matches snapshot', async () => {
+    it('Renders employee data when successfully fetching and matches snapshot', async () => {
         global.fetch = jest.fn().mockResolvedValue({
             json: () => mockData,
         })
@@ -17,6 +17,24 @@ describe('<Fetch />', () => {
         expect(global.fetch).toHaveBeenCalled()
 
         expect(fetchComponent).toMatchSnapshot()
+        global.fetch.mockReset()
+    })
+
+    it('Renders an error message when fetch fails and matches snapshot', async () => {
+        const errorMsg = 'There has been an error...'
+        const mockError = { error: errorMsg }
+        global.fetch = jest.fn().mockResolvedValue({
+            json: () => mockError,
+        })
+        const wrapper = mount(<App />)
+        await wait()
+        wrapper.update()
+        const fetchComponent = wrapper.find(Fetch)
+        console.log(fetchComponent.debug())
+        expect(global.fetch).toHaveBeenCalled()
+
+        expect(fetchComponent).toMatchSnapshot()
+        expect(fetchComponent.find('p').text()).toEqual(errorMsg)
         global.fetch.mockReset()
     })
 })
